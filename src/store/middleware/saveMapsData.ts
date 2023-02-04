2561ba2fa14c5dfdc48c942a2823c4bd869e0283
Danil <danylo.makhov@nure.ua>
2023-02-04 19:49:46 +0300

    feat: add new custom hooks

---
+++ src/store/middleware/saveMapsData.ts
@@ -0,0 +1,19 @@
+import { Dispatch } from 'redux';
+import { userDataState } from '../../types/userDataTypes/userData';
+import IMapData from '../../types/mapsDataTypes/mapsDataTypes';
+
+const saveMapsData = (store: IMapData[]) => (next: Dispatch) => (action: {
+  type: string; payload: {
+    mapName: string;
+    audio: string;
+    albumCover: string[];
+    topPlayers: string[];
+    additionalAudio: string[];
+    additionalPictures: string[];
+  };
+}) => {
+  // тут сохранять данные пользователя при перезагрузке
+  const result = next(action);
+  return result;
+};
+export default saveMapsData;
