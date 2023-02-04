1f654754b876c42add4077289bb6dedc9e2e3b86
Ayrat <axmetxan@gmail.com>
2023-02-04 19:43:16 +0300

    feat: add maps data console output

--- src/pages/selectMap/SelectMap.tsx
+++ src/pages/selectMap/SelectMap.tsx
@@ -1,14 +1,19 @@
+/* eslint-disable import/order */
 import React, { useRef, useEffect, useState } from 'react';
 import parallaxCreate from '../../utils/parallaxCreate';
 import './SelectMapPageStyles.scss';
 import throttle from '../../utils/throttle';
 import songsData from './songsData';
 import SongListItem from '../../components/selectMap/songList/SongListItem';
+import { useSelector } from 'react-redux';
+import IReducers from '../../types/reducers/reducersType';
 
 function SelectMap() {
   const throttleInProgress = useRef(false);
   const [clickedSongListItemID, setClickedSongListItemID] = useState('1011011');
   const [backgroundSource, setBackgroundSource] = useState(songsData[1011011].background);
+  const mapsData = useSelector((state: IReducers) => state.mapsDataReducer);
+  console.log(mapsData);
 
   useEffect(() => {
     const background = document.querySelector('.select-map-page-container__background') as HTMLDivElement;
