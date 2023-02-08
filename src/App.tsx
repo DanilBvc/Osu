11fff2b973287317cda49b83db4ff5bc898a0a45
Andrey <spacecmetanka@gmail.com>
2023-02-08 22:10:46 +0300

    refactor: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,34 @@
-import React from 'react';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import MainPage from './pages/mainPage/MainPage';
 import NotFound from './pages/notFound/NotFound';
 import Game from './pages/Game/Game';
 import SelectMap from './pages/selectMap/SelectMap';
 import './globalStyles/global.scss';
+import PrivateRoutes from './utils/RequireAuth/PrivatRoutes';
+import useUnSub from './customHooks/useUnSub';
+import useGetMapsData from './customHooks/useGetMapsData';
+
+import AuthPopup from './components/authPopup/AuthPopup';
+import IReducers from './types/reducers/reducersType';
 
 function App() {
+  useUnSub();
+  useGetMapsData();
+  const isAuth = useSelector((state: IReducers) => !!state.userDataReducer.email);
   return (
-    <Routes>
-      <Route path="/" element={<MainPage />} />
-      <Route path="/404" element={<NotFound />} />
-      <Route path="/game" element={<Game />} />
-      <Route path="/selectMap" element={<SelectMap />} />
-    </Routes>
+    <>
+      {!isAuth ? <AuthPopup /> : null}
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/404" element={<NotFound />} />
+        <Route element={<PrivateRoutes />}>
+          <Route path="/game" element={<Game />} />
+          <Route path="/selectMap" element={<SelectMap />} />
+        </Route>
+      </Routes>
+    </>
   );
 }
-
 export default App;
