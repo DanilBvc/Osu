e7b0eec21fe445dcea048afac1ee4212894b5d13
Andrey <spacecmetanka@gmail.com>
2023-02-08 22:10:12 +0300

    refactor: main page remove unused imports

--- src/pages/mainPage/MainPage.tsx
+++ src/pages/mainPage/MainPage.tsx
@@ -1,30 +1,15 @@
-/* eslint-disable import/order */
-/* eslint-disable max-len */
-import React, { useEffect } from 'react';
-import { onAuthStateChanged, signOut } from 'firebase/auth';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import Footer from '../../components/footer/Footer';
 import InfoPanel from '../../components/infoPanel/InfoPanel';
-
 import './mainPage.scss';
-import {
-  collection, doc, getDoc, getDocs
-} from 'firebase/firestore';
-import LoginComponent from '../../components/Login/LoginComponent';
-import useAuth from '../../customHooks/useAuth';
-import { auth, db } from '../../firebase/firebase';
-import setUserData from '../../store/actionCreators/userData/setUserData';
-import AddMap from '../../components/addMap/AddMap';
-import setNewMap from '../../store/actionCreators/mapsData/setNewMap';
-import RegisterCmponents from '../../components/Register/RegisterCmponents';
-import useUnSub from '../../customHooks/useUnSub';
-import useGetMapsData from '../../customHooks/useGetMapsData';
+
 import IReducers from '../../types/reducers/reducersType';
 import BigButton from '../../components/bigButton/BigButton';
 
 function MainPage() {
+  const isAuth = useSelector((state: IReducers) => !!state.userDataReducer.email);
   return (
-    <main className="main">
+    <main className="main" style={!isAuth ? { pointerEvents: 'none' } : {}}>
       <InfoPanel />
       <BigButton />
       <Footer />
