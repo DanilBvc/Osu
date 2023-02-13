5dd406bd6db51c4651b635af5b60df1a9ff5210a
Ayrat <axmetxan@gmail.com>
2023-02-13 23:29:41 +0300

    fix: mouse leave element unselect

--- src/components/selectMap/songList/songListMousLeaveHandler.ts
+++ src/components/selectMap/songList/songListMousLeaveHandler.ts
@@ -1,12 +1,18 @@
 const songListMousLeaveHandler = (
   event: React.MouseEvent,
-  clickedSongListItemID: string
+  clickedSongListItemID: string,
+  selectedSongDifficulty: string
 ) => {
   const songListItem = event.currentTarget.closest('.song-list-item');
+
   if (songListItem instanceof HTMLElement) {
     const songListItemID = String(songListItem.getAttribute('data-id'));
+    const songListItemDiffuculty = String(songListItem.getAttribute('data-difficulty'));
 
-    if (songListItemID !== clickedSongListItemID) {
+    if (
+      songListItemID !== clickedSongListItemID
+      || selectedSongDifficulty !== songListItemDiffuculty
+    ) {
       songListItem.classList.remove('active-item');
     }
     songListItem.classList.remove('stick-selected-item');
