0cc10938affda3723e41036f0c0a08272547566a
Ayrat <axmetxan@gmail.com>
2023-02-13 23:28:00 +0300

    feat: add audio context

---
+++ src/contexts/audioContextWrapper.tsx
@@ -0,0 +1,65 @@
+import React, {
+  createContext, useContext, useMemo, useRef, useState
+} from 'react';
+
+const ReactAudioContext = createContext<{
+  audioContext: AudioContext | null;
+  audioAnalyser: AnalyserNode | null;
+  playAudio(source: string): void;
+} | null>(null);
+const audioElement = new Audio();
+
+export function AudioContextWrapperProvider({ children }: {children: React.ReactNode}) {
+  const audioElementRef = useRef<HTMLAudioElement>(audioElement);
+  const audioSourceRef = useRef('');
+  const audioContextRef = useRef<AudioContext | null>(null);
+  const [audioAnalyser, setAudioAnalyser] = useState<AnalyserNode | null>(null);
+  const value = useMemo(() => {
+    let mediaElementSource: MediaElementAudioSourceNode | null = null;
+
+    function playAudio(audioSource: string): void {
+      const audioContext = audioContextRef.current ?? new AudioContext();
+
+      if (audioSourceRef.current === audioSource) {
+        return;
+      }
+      audioContextRef.current = audioContext;
+      audioElementRef.current?.pause();
+      audioElementRef.current.src = audioSource;
+      audioElementRef.current.crossOrigin = 'anonymous';
+      if (!mediaElementSource) {
+        const newAudioAnalyser = audioContext.createAnalyser();
+
+        mediaElementSource = audioContext.createMediaElementSource(audioElementRef.current);
+        mediaElementSource.connect(newAudioAnalyser);
+        newAudioAnalyser.connect(audioContext.destination);
+        setAudioAnalyser(newAudioAnalyser);
+      }
+      audioElementRef.current.play();
+      audioSourceRef.current = audioSource;
+    }
+
+    return { audioContext: audioContextRef.current, playAudio };
+  }, []);
+  const providerValues = useMemo(() => ({ ...value, audioAnalyser }), [value, audioAnalyser]);
+
+  return (
+    <ReactAudioContext.Provider value={providerValues}>
+      {children}
+    </ReactAudioContext.Provider>
+  );
+}
+
+export const useAudioContext = () => useContext(ReactAudioContext);
+
+export const useAudioAanalyser = () => {
+  const audioContext = useAudioContext();
+
+  return audioContext?.audioAnalyser;
+};
+
+export const usePlayAudio = () => {
+  const audioContext = useAudioContext();
+
+  return audioContext?.playAudio;
+};
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './store/store';
+import { AudioContextWrapperProvider } from './contexts/audioContextWrapper';
 // eslint-disable-next-line import/no-unresolved
 
 const root = ReactDOM.createRoot(
@@ -12,7 +13,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <BrowserRouter>
     <Provider store={store}>
-      <App />
+      <AudioContextWrapperProvider>
+        <App />
+      </AudioContextWrapperProvider>
     </Provider>
   </BrowserRouter>
 );
