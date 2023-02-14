93d6e948d83428fd4af041437d7cdb2434be4cfe
Andrey <spacecmetanka@gmail.com>
2023-02-15 00:50:32 +0300

    feat: hook for scale game resolution

--- src/customHooks/useClientResolution.ts
+++ src/customHooks/useClientResolution.ts
@@ -1,17 +1,12 @@
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { setGameResolution } from '../store/reducers/game/gameOptionsReducer';
-import { HitObjects } from '../types/mapsDataTypes/osuDataTypes';
-import IReducers from '../types/reducers/reducersType';
 
 const useClientResolution = () => {
-  const { hitObjects } = useSelector(
-    (state: IReducers) => state.mapsDataReducer[0].mapData[0] // 0 = захаркоженная сложность
-  );
   const dispatch = useDispatch();
 
   const resolutionCoefficients = {
-    CX: +(window.innerWidth / Math.max(...hitObjects.map((obj: HitObjects) => obj.x))).toFixed(0),
-    CY: +(window.innerHeight / Math.max(...hitObjects.map((obj: HitObjects) => obj.y))).toFixed(0),
+    CX: +(window.innerWidth / 512).toFixed(2),
+    CY: +(window.innerHeight / 384).toFixed(2),
   };
   dispatch(setGameResolution(resolutionCoefficients));
 };
