269a04892b316403ca418cf00498ce4cb25c9a17
Andrey <spacecmetanka@gmail.com>
2023-02-15 00:57:06 +0300

    refactor: app file  refactor

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,19 @@ import { Route, Routes } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import MainPage from './pages/mainPage/MainPage';
 import NotFound from './pages/notFound/NotFound';
-import Game from './pages/Game/Game';
 import SelectMap from './pages/selectMap/SelectMap';
 import './globalStyles/global.scss';
 import PrivateRoutes from './utils/RequireAuth/PrivatRoutes';
 import useUnSub from './customHooks/useUnSub';
-
 import AuthPopup from './components/authPopup/AuthPopup';
 import IReducers from './types/reducers/reducersType';
+import useClientResolution from './customHooks/useClientResolution';
+
+import Game from './pages/Game/Game';
 
 function App() {
   useUnSub();
-
+  useClientResolution();
   const isAuth = useSelector((state: IReducers) => !!state.userDataReducer.email);
   return (
     <>
