16a429f2dcac57147ed7303fab96c49a9c5ebdfd
Ayrat <axmetxan@gmail.com>
2023-02-14 20:35:42 +0300

    feat: fix mainPage footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,25 +4,31 @@ import './footer.scss';
 export default function Footer(): JSX.Element {
   return (
     <footer className="footer">
-      <ul className="authors">
-        <li className="author">
-          <a target="_blank" href="https://github.com/DanilBvc" className="author-link" rel="noreferrer">
-            <GithubSvg />
-          </a>
-        </li>
-        <li className="author">
-          <a target="_blank" href="https://github.com/groom7" className="author-link" rel="noreferrer">
-            <GithubSvg />
-          </a>
-        </li>
-        <li className="author">
-          <a target="_blank" href="https://github.com/AndreyVeres" className="author-link" rel="noreferrer">
-            <GithubSvg />
-          </a>
-        </li>
-      </ul>
-
-      <p>{new Date().getFullYear()}</p>
+      <div className="outer-wrapper">
+        <div className="socials-links">
+          <div className="github-block">
+            <a className="image-link github-block__link hover-opacity" href="https://github.com/DanilBvc">
+              <GithubSvg />
+            </a>
+          </div>
+          <div className="github-block">
+            <a className="image-link github-block__link hover-opacity" href="https://github.com/groom7">
+              <GithubSvg />
+            </a>
+          </div>
+          <div className="github-block">
+            <a className="image-link github-block__link hover-opacity" href="https://github.com/AndreyVeres">
+              <GithubSvg />
+            </a>
+          </div>
+          <div className="rss-block">
+            <a className="image-link rss-block__link hover-opacity" href="https://rs.school/js/">
+              <img className="rss-block__logo" src="https://rs.school/images/rs_school_js.svg" alt="rs.school logo" />
+            </a>
+          </div>
+        </div>
+        <div className="developed-year">2023</div>
+      </div>
     </footer>
 
   );
