70281ab3b29b9b38a9ebd4ce3db7f732679ed224
Ayrat <axmetxan@gmail.com>
2023-02-15 13:54:10 +0300

    fix: implement request maps data and avoid exist maps data dispatch

--- src/pages/mainPage/MainPage.tsx
+++ src/pages/mainPage/MainPage.tsx
@@ -1,4 +1,5 @@
-import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Footer from '../../components/footer/Footer';
 import InfoPanel from '../../components/infoPanel/InfoPanel';
 import './mainPage.scss';
@@ -6,9 +7,29 @@ import IReducers from '../../types/reducers/reducersType';
 import BigButton from '../../components/bigButton/BigButton';
 import ParallaxBackground from '../../components/selectMap/parallaxBacground/ParallaxBackground';
 import OsuButton from '../../components/selectMap/osuButton/OsuButton';
+import setNewMap from '../../store/actionCreators/mapsData/setNewMap';
+import getMapsData from '../../utils/api/getMapsData';
 
 function MainPage() {
+  const dispatch = useDispatch();
   const isAuth = useSelector((state: IReducers) => !!state.userDataReducer.email);
+  const storeMapsData = useSelector((state: IReducers) => state.mapsDataReducer);
+
+  useEffect(() => {
+    getMapsData().then((mapsData) => mapsData.forEach((mapData) => {
+      if (storeMapsData.findIndex((storeMapData) => storeMapData.id === mapData.id) === -1) {
+        dispatch(setNewMap({
+          mapName: mapData.mapName,
+          audio: mapData.audio,
+          images: mapData.images,
+          topPlayers: ['andrew', 'grisha', 'billy'],
+          additionalAudio: mapData.additionAudio,
+          id: mapData.id,
+          mapData: mapData.mapData,
+        }));
+      }
+    }));
+  }, []);
 
   return (
     <main className="main" style={!isAuth ? { pointerEvents: 'none' } : {}}>
@@ -20,7 +41,6 @@ function MainPage() {
       <BigButton />
       <Footer />
     </main>
-
   );
 }
 
---
+++ src/utils/api/getMapsData.ts
@@ -0,0 +1,50 @@
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from '../../firebase/firebase';
+import { MapDataFromApi } from '../../types/mapsDataTypes/mapsDataFromApiTypes';
+
+const getMapsData = async () => {
+  const querySnapshot = await getDocs(collection(db, 'maps'));
+  const maps: MapDataFromApi[] = [];
+
+  querySnapshot.forEach((document) => {
+    const mapsData = document.data();
+    const resultData: MapDataFromApi = {
+      id: '',
+      mapName: '',
+      audio: '',
+      additionAudio: [],
+      images: [],
+      mapData: [],
+    };
+
+    Object.entries(mapsData).forEach((data) => {
+      const format = data[0].split(' ')[0];
+      const name = data[0].split(' ')[1];
+      const fileLink: string = data[1];
+      if (format === 'audio') {
+        resultData.additionAudio = [...resultData.additionAudio, {
+          audioName: name,
+          audioFile: fileLink,
+        }];
+      } else if (format === 'id') {
+        resultData.id = fileLink;
+      } else if (format === 'images') {
+        resultData.images = [...resultData.images, { imagesName: name, imagesFile: fileLink }];
+      } else if (format === 'mapData') {
+        resultData.mapData = [...resultData.mapData, JSON.parse(fileLink)];
+      }
+    });
+    resultData.mapName = resultData.mapData[0].metadata.Title;
+    Object.entries(resultData.additionAudio).forEach((item) => {
+      const format = resultData.mapData[0].general.AudioFilename.split('.').join('');
+      if (item[1].audioName === format) {
+        resultData.audio = item[1].audioFile;
+      }
+    });
+    maps.push(resultData);
+  });
+
+  return maps;
+};
+
+export default getMapsData;
