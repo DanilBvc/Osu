675a001a28cd23bfcfcada564ac931a017fe8d8b
Andrey <spacecmetanka@gmail.com>
2023-02-17 15:59:26 +0300

    refactor: slider component

--- src/pages/Game/Slider/Slider.tsx
+++ src/pages/Game/Slider/Slider.tsx
@@ -1,37 +1,26 @@
 import {
-  useCallback, useEffect, useRef, useState
+  useCallback,
+  useEffect,
+  useRef, useState
 } from 'react';
 import { Group, Line, Circle } from 'react-konva';
 import Konva from 'konva';
 import { IGameElement } from '../../../types/gameTypes';
-import { handleCursor } from '../../../utils/gameCursorHandler';
+import { handleCursor } from '../../../utils/game/gameCursorHandler';
+import { defaultColors, defaultKeyframes } from '../../../utils/game/defaultValues';
+import RadiusRing from '../RadiusRing/RadiusRing';
 
 export default function GameSlider({
   colors,
   model,
   audioRef,
-}: IGameElement) {
+}: IGameElement): JSX.Element {
   const startPoint = { x: model.x, y: model.y };
-  const bezierPoints = [startPoint, ...model.keyframes!];
+  const bezierPoints = [startPoint, ...model.keyframes ?? defaultKeyframes];
   const circleRef = useRef<Konva.Circle | null>(null);
   const groupRef = useRef<Konva.Group | null>(null);
-  const radiusRef = useRef<Konva.Circle | null>(null);
-  const [ballColor, ballColor2, trackColor, borderColor] = colors;
+  const [ballColor1, trackColor, borderColor] = colors || defaultColors;
   const [visible, setVisible] = useState(false);
-
-  const lifeTime = 2000; // хардкод
-  const duration = 2; // хардкод
-
-  const startAnimationRadius = () => {
-    if (radiusRef.current) {
-      radiusRef.current.to({
-        duration: 0.7,
-        radius: 30,
-        easing: Konva.Easings.Linear,
-        onFinish: () => radiusRef.current?.destroy(),
-      });
-    }
-  };
 
   const startAnimationHandler = useCallback(() => {
     const startAnimationBall = (points = bezierPoints, index = 0) => {
@@ -43,7 +32,7 @@ export default function GameSlider({
         circleRef.current.to({
           x: points[index].x,
           y: points[index].y,
-          duration: duration / points.length,
+          duration: (model.animationTime / 1000) / points.length,
           easing: Konva.Easings.Linear,
           onFinish: () => startAnimationBall(points, index + 1),
         });
@@ -57,10 +46,9 @@ export default function GameSlider({
     if (audioRef.current) {
       const currentTime = +(audioRef.current.currentTime * 1000).toFixed(0);
 
-      if (currentTime > model.time && currentTime < model.time + lifeTime) {
+      if (currentTime > model.time && currentTime < model.time + model.animationTime) {
         setVisible(true);
         startAnimationHandler()();
-        startAnimationRadius();
         audioRef.current?.removeEventListener('timeupdate', timeUpdate);
       } else setVisible(false);
     }
@@ -72,31 +60,25 @@ export default function GameSlider({
 
   return (
     <Group ref={groupRef} visible={visible}>
-      <Line
+      <Line // border
         points={bezierPoints.flatMap(({ x, y }) => [x, y])}
         lineCap="round"
-        stroke={`rgb(${borderColor.join(',')})`}
-        strokeWidth={90}
-        opacity={0.5}
-        shadowColor={`rgb(${borderColor.join(',')})`}
+        stroke={`rgb(${borderColor?.join(',')})`}
+        strokeWidth={100}
+        opacity={0.3}
+        shadowColor={`rgb(${borderColor?.join(',')})`}
         shadowBlur={10}
       />
       <Line // track
         points={bezierPoints.flatMap(({ x, y }) => [x, y])}
         lineCap="round"
         stroke={`rgb(${trackColor.join(',')})`}
-        strokeWidth={80}
+        strokeWidth={90}
         shadowColor={`rgb(${trackColor.join(',')})`}
         shadowBlur={30}
-        opacity={0.5}
+        opacity={0.3}
       />
-      <Circle // radius
-        ref={radiusRef}
-        radius={100}
-        x={startPoint.x}
-        y={startPoint.y}
-        stroke="red"
-      />
+      <RadiusRing x={startPoint.x} y={startPoint.y} visible={visible} />
       <Circle // ball
         onClick={startAnimationHandler}
         onMouseEnter={(e) => handleCursor(e)}
@@ -104,13 +86,15 @@ export default function GameSlider({
         onMouseLeave={(e) => handleCursor(e)}
         x={startPoint.x}
         y={startPoint.y}
-        radius={35}
-        shadowBlur={10}
+        radius={40}
         ref={circleRef}
-        fill={`rgb(${ballColor2.join(',')})`}
-        stroke={`rgb(${ballColor.join(',')})`}
-        shadowColor={`rgb(${ballColor2.join(',')})`}
-        strokeWidth={2}
+        fill={`rgb(${ballColor1.join(',')})`}
+        strokeWidth={1}
+        opacity={1}
+        shadowColor="black"
+        shadowOffset={{ x: 5, y: 5 }}
+        shadowBlur={10}
+        shadowOpacity={0.8}
       />
     </Group>
   );
