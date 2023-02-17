28232793855b22702bf21b1c83af094b0b1e1d36
Andrey <spacecmetanka@gmail.com>
2023-02-17 15:59:16 +0300

    refactor : circle component

--- src/pages/Game/Circle/Circle.tsx
+++ src/pages/Game/Circle/Circle.tsx
@@ -1,33 +1,35 @@
 import Konva from 'konva';
 import { useEffect, useRef, useState } from 'react';
 import { Circle, Group } from 'react-konva';
+import { useDispatch } from 'react-redux';
 import { IGameElement } from '../../../types/gameTypes';
-import { handleCursor } from '../../../utils/gameCursorHandler';
+import { defaultColors } from '../../../utils/game/defaultValues';
+import { handleCursor } from '../../../utils/game/gameCursorHandler';
+import RadiusRing from '../RadiusRing/RadiusRing';
 
 export default function GameCircle({
   model,
   colors,
   audioRef,
-}: IGameElement) {
+}: IGameElement): JSX.Element {
   const circleRef = useRef<Konva.Circle | null>(null);
-  const [borderColor, fillColor] = colors;
-  const { x, y, time } = model;
-  const radius = 40; // хардкод
+  const [fillColor] = colors || defaultColors;
+  const {
+    x, y, time, animationTime,
+  } = model;
   const [visible, setVisible] = useState(false);
-
-  const lifeTime = 1000; // хардкод
 
   const timeUpdate = () => {
     if (audioRef.current) {
       const currentTime = +(audioRef.current.currentTime * 1000).toFixed(0);
 
-      if (currentTime > time && currentTime < time + lifeTime) {
+      if (currentTime > time && currentTime < time + animationTime) {
         setVisible(true);
         audioRef.current?.removeEventListener('timeupdate', timeUpdate);
 
         setTimeout(() => {
           circleRef.current?.destroy();
-        }, lifeTime);
+        }, animationTime);
       } else setVisible(false);
     }
   };
@@ -36,18 +38,27 @@ export default function GameCircle({
     audioRef.current?.addEventListener('timeupdate', timeUpdate);
   }, []);
 
+  const dispatch = useDispatch();
+  // const scoreHandler = () => {
+
+  // }
+
   return (
     <Group visible={visible}>
+      <RadiusRing x={x} y={y} visible={visible} />
       <Circle
         onClick={() => circleRef.current?.destroy()}
         onMouseEnter={(e) => handleCursor(e)}
         onPointerClick={(e) => handleCursor(e)}
         x={x}
         y={y}
-        stroke={`rgba(${borderColor.join(',')},1)`}
-        radius={radius}
-        fill={`rgba(${fillColor.join(',')},1)`}
+        radius={40}
+        fill={`rgba(${fillColor?.join(',')},1)`}
         ref={circleRef}
+        shadowColor="black"
+        shadowOffset={{ x: 5, y: 5 }}
+        shadowBlur={10}
+        shadowOpacity={0.8}
       />
     </Group>
   );
