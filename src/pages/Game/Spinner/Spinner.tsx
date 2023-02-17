350545a8b783e20978fe650128c65b89eaa96122
Andrey <spacecmetanka@gmail.com>
2023-02-17 15:59:01 +0300

    feat: spinner component

--- src/pages/Game/Spinner/Spinner.tsx
+++ src/pages/Game/Spinner/Spinner.tsx
@@ -1,29 +1,69 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-
-import React from 'react';
-import { Circle, Arc, Group } from 'react-konva';
+import { useEffect, useState } from 'react';
+import {
+  Circle
+} from 'react-konva';
+import { KonvaEventObject } from 'konva/lib/Node';
 import { IGameElement } from '../../../types/gameTypes';
+import spinnerMesh from '../../../assets/mesh/spinner.png';
 
-function Spinner({ spinner }: any) {
-  const endAngle = (360 * (spinner.endTime - spinner.time)) / (spinner.endTime - spinner.time);
+function Spinner({
+  model,
+  audioRef,
+}: IGameElement): JSX.Element {
+  const image = new window.Image();
+  image.src = spinnerMesh;
+  const [rotation, setRotation] = useState<number>(0);
+  const [holding, setHolding] = useState<boolean>(false);
+  const [visible, setVisible] = useState<boolean>(false);
+  const lifeTime = 5000;
+
+  const handleMouseMove = (event: KonvaEventObject<MouseEvent>) => {
+    if (holding) {
+      const stage = event.target.getStage();
+      const pointerPos = stage?.getPointerPosition();
+      if (stage && pointerPos) {
+        const stagePos = {
+          x: stage.width() / 2,
+          y: stage.height() / 2,
+        };
+        const dx = pointerPos.x - stagePos.x;
+        const dy = pointerPos.y - stagePos.y;
+        const angle = Math.atan2(dy, dx);
+        const rotationDeg = (angle * 180) / Math.PI;
+        setRotation(rotationDeg);
+      }
+    }
+  };
+
+  const timeUpdate = () => {
+    if (audioRef.current) {
+      const currentTime = +(audioRef.current.currentTime * 1000).toFixed(0);
+
+      if (currentTime > model.time && currentTime < model.time + lifeTime) {
+        setVisible(true);
+      } else setVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    audioRef.current?.addEventListener('timeupdate', timeUpdate);
+  }, []);
+
   return (
-    <Group x={spinner.x} y={spinner.y}>
-      <Circle
-        radius={84}
-        stroke="#ffffff"
-        strokeWidth={4}
-        dash={[8, 8]}
-        dashOffset={16}
-        rotation={90}
-      />
-      <Arc
-        angle={endAngle}
-        rotation={-90}
-        innerRadius={0}
-        outerRadius={84}
-        fill="#ffffff"
-      />
-    </Group>
+    <Circle
+      visible={visible}
+      x={window.innerWidth / 2}
+      y={window.innerHeight / 2}
+      radius={300}
+      fillPatternImage={image}
+      fillPatternScale={{ x: 0.9, y: 0.9 }}
+      fillPatternOffsetX={333}
+      fillPatternOffsetY={333}
+      rotation={rotation}
+      onMouseMove={(e) => handleMouseMove(e)}
+      onMouseDown={() => setHolding(true)}
+      onMouseUp={() => setHolding(false)}
+    />
   );
 }
 
