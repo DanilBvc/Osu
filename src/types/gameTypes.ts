0b37f277eee3fb7c0ea3e138ddc4267092e1af04
Andrey <spacecmetanka@gmail.com>
2023-02-21 23:51:42 +0300

    refactor: game types

--- src/types/gameTypes.ts
+++ src/types/gameTypes.ts
@@ -1,18 +1,28 @@
+import { Layer } from 'konva/lib/Layer';
 import { HitObjects } from './mapsDataTypes/osuDataTypes';
 
 export interface UpdatedObject extends HitObjects {
   unKey: string;
   animationTime: number;
   keyframes: { x: number; y: number }[] | undefined;
-}
-export interface IGameElement {
-  model: UpdatedObject;
-  colors?: number[][];
-  audioRef: React.RefObject<HTMLAudioElement>;
+
 }
 
 export interface IRadiusRingProps {
   x: number;
   y: number;
-  visible: boolean;
+}
+
+export interface IResultMessage {
+  x: number;
+  y: number;
+  color: string;
+  text: number | string;
+  id?: string;
+}
+export interface IGameElement {
+  model: UpdatedObject;
+  colors?: number[][];
+  layerRef: React.RefObject<Layer>;
+  messageHandler?: (result: IResultMessage) => void;
 }
