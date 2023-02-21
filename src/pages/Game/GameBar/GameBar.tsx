bc940e4fc3a4ab46b526e5ea4b106dab2903fe5f
Andrey <spacecmetanka@gmail.com>
2023-02-21 23:55:06 +0300

    refactor: gamebar

--- src/pages/Game/GameBar/GameBar.tsx
+++ src/pages/Game/GameBar/GameBar.tsx
@@ -4,7 +4,6 @@ import './gameBar.scss';
 
 export default function GameBar(): JSX.Element {
   const { accuracy, points } = useSelector((state: IReducers) => state.gameScoreReducer);
-
   return (
     <div className="game-bar">
       <p className="accuracy">
