3ea4a35183b6bf8898c8e7a84461f91be65a2efe
Andrey <spacecmetanka@gmail.com>
2023-02-21 23:52:06 +0300

    feat: off cursor handler

--- src/utils/game/gameCursorHandler.ts
+++ src/utils/game/gameCursorHandler.ts
@@ -3,14 +3,14 @@ import { KonvaEventObject } from 'konva/lib/Node';
 export const handleCursor = (e: KonvaEventObject<MouseEvent>) => {
   const stage = e.target.getStage();
   if (stage) {
-    if (e.type === 'pointerclick') {
-      stage.container().style.cursor = 'default';
-    }
-    if (e.type === 'mouseleave') {
-      stage.container().style.cursor = 'default';
-    }
-    if (e.type === 'mouseenter') {
-      stage.container().style.cursor = 'pointer';
-    }
+    // if (e.type === 'pointerclick') {
+    //   stage.container().style.cursor = 'default';
+    // }
+    // if (e.type === 'mouseleave') {
+    //   stage.container().style.cursor = 'default';
+    // }
+    // if (e.type === 'mouseenter') {
+    //   stage.container().style.cursor = 'pointer';
+    // }
   }
 };
