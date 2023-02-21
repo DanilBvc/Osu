9f8db0d42c49d0424956d9d83f3f83962759d8d2
Andrey <spacecmetanka@gmail.com>
2023-02-21 23:52:35 +0300

    refactor : move render in other component

--- src/pages/Game/Game.tsx
+++ src/pages/Game/Game.tsx
@@ -1,29 +1,31 @@
 /* eslint-disable max-len */
 import { useRef, useEffect } from 'react';
-import { Stage, Layer } from 'react-konva';
-import { useSelector } from 'react-redux';
-import GameCircle from './Circle/Circle';
-import GameSlider from './Slider/Slider';
-import Spinner from './Spinner/Spinner';
+import { Stage, Layer, Circle } from 'react-konva';
+import { useDispatch, useSelector } from 'react-redux';
+import Konva from 'konva';
 import IReducers from '../../types/reducers/reducersType';
 import './game.scss';
 import audioPlug from '../../assets/plugs/audio-plug.mp3';
 import useUpdate from '../../customHooks/useUpdate';
-import { UpdatedObject } from '../../types/gameTypes';
 import GameBar from './GameBar/GameBar';
+import { resetGameAction } from '../../store/reducers/game/gameScoreReducer';
+import HitObjects from './hitObjects';
+import Preloader from './Preloader/Preloader';
 
-function Game(): JSX.Element {
+export default function Game(): JSX.Element {
   const mapData = useSelector((state: IReducers) => state.activeGameReduccer);
-
-  const {
-    ApproachRate, OverallDifficulty, SliderMultiplier, SliderTickRate,
-  } = mapData.mapData[0].difficulty;
-  const { hitObjects, timingPoints, colors } = mapData.mapData[0];
-
-  const gameElements = useUpdate(hitObjects, timingPoints, ApproachRate, OverallDifficulty, 0.3);
   const audioRef = useRef<HTMLAudioElement>(null);
+  const { ApproachRate, OverallDifficulty } = mapData.mapData[0].difficulty;
+  const { hitObjects, timingPoints, colors } = mapData.mapData[0];
+  const dispath = useDispatch();
+  dispath(resetGameAction());
+  const layerRef = useRef<Konva.Layer>(null);
+  const gameElements = useUpdate(hitObjects, timingPoints, ApproachRate, OverallDifficulty, 0.5);
+
   useEffect(() => {
-    if (audioRef.current) audioRef.current.play();
+    if (audioRef.current) {
+      audioRef.current.play();
+    }
   }, []);
 
   return (
@@ -34,49 +36,54 @@ function Game(): JSX.Element {
       }
     >
       <GameBar />
+
       <audio ref={audioRef} src={mapData.audio || audioPlug}>
         <track kind="captions" />
       </audio>
-      <Stage
-        width={window.innerWidth}
-        height={window.innerHeight}
-      >
-        <Layer>
-          {gameElements.map((object: UpdatedObject) => {
-            if (object.type === 'slider') {
-              return (
-                <GameSlider
-                  key={object.unKey}
-                  model={object}
-                  colors={colors}
-                  audioRef={audioRef}
-                />
-              );
-            }
-            if (object.type === 'circle') {
-              return (
-                <GameCircle
-                  key={object.unKey}
-                  colors={colors}
-                  model={object}
-                  audioRef={audioRef}
-                />
-              );
-            }
-            return (
-              <Spinner
-                key={window.crypto.randomUUID()}
-                model={object}
-                colors={colors}
-                audioRef={audioRef}
-              />
-            );
-          })}
+
+      <Stage width={window.innerWidth} height={window.innerHeight}>
+        <Layer ref={layerRef}>
+          {/* <Preloader /> */}
+          <HitObjects objects={gameElements} colors={colors} audioRef={audioRef} layerRef={layerRef} />
         </Layer>
       </Stage>
     </div>
-
   );
 }
 
-export default Game;
+// const star = new Konva.Star({
+//   x: 500,
+//   y: 500,
+//   numPoints: 5,
+//   innerRadius: 10,
+//   outerRadius: 20,
+//   fill: 'red',
+// });
+
+// const starRef = useRef(star);
+
+// const onMouseMove = (e: KonvaEventObject<MouseEvent>) => {
+//   const { clientX, clientY } = e.evt;
+
+//   // star.x(clientX - 10);
+//   // star.y(clientY - 10);
+//   // mouseCircle.x(clientX);
+//   // mouseCircle.y(clientY);
+// };
+
+// useEffect(() => {
+//   const animation = new Konva.Animation((frame: any) => {
+//     if (starRef.current) {
+//       const x = frame.time * 0.01 % window.innerWidth;
+//       const y = frame.time * 0.01 % window.innerHeight;
+//       starRef.current.x(x);
+//       starRef.current.y(y);
+//     }
+//   }, starRef.current?.getLayer());
+
+//   animation.start();
+
+//   return () => {
+//     animation.stop();
+//   };
+// }, []);
