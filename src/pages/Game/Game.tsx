9a72c4c66fc0a4c9e7fdda61d43b9055a6184774
Andrey <spacecmetanka@gmail.com>
2023-02-25 23:21:54 +0300

    refactor: game stage

--- src/pages/Game/Game.tsx
+++ src/pages/Game/Game.tsx
@@ -1,8 +1,8 @@
-/* eslint-disable max-len */
 import { useRef, useEffect } from 'react';
 import { Stage, Layer } from 'react-konva';
 import { useDispatch, useSelector } from 'react-redux';
 import Konva from 'konva';
+
 import IReducers from '../../types/reducers/reducersType';
 import './game.scss';
 import audioPlug from '../../assets/plugs/audio-plug.mp3';
@@ -18,12 +18,21 @@ export default function Game(): JSX.Element {
   const mainPlayerAudioElement = useAudioElement();
   const gameId = useSelector((state: IReducers) => state.songDifficultyIndexReducer);
   const audioRef = useRef<HTMLAudioElement>(null);
+  const preloaderDelay = 2000;
+  const speedMultiplier = 0.4;
   const { ApproachRate, OverallDifficulty } = mapData.mapData[gameId].difficulty;
   const { hitObjects, timingPoints, colors } = mapData.mapData[gameId];
   const dispatch = useDispatch();
   dispatch(resetGameAction());
   const layerRef = useRef<Konva.Layer>(null);
-  const gameElements = useUpdate(hitObjects, timingPoints, ApproachRate, OverallDifficulty, 0.5);
+  const gameElements = useUpdate(
+    hitObjects,
+    timingPoints,
+    ApproachRate,
+    OverallDifficulty,
+    speedMultiplier,
+    preloaderDelay
+  );
 
   useEffect(() => {
     if (mainPlayerAudioElement) {
@@ -49,8 +58,13 @@ export default function Game(): JSX.Element {
 
       <Stage width={window.innerWidth} height={window.innerHeight}>
         <Layer ref={layerRef}>
-          {/* <Preloader /> */}
-          <HitObjects objects={gameElements} colors={colors} audioRef={audioRef} layerRef={layerRef} />
+          <Preloader animationTime={preloaderDelay} />
+          <HitObjects
+            objects={gameElements}
+            colors={colors}
+            audioRef={audioRef}
+            layerRef={layerRef}
+          />
         </Layer>
       </Stage>
     </div>
