6d15d991be70cc184de888806ed58d81e7c4e1dd
Danil <danylo.makhov@nure.ua>
2023-02-26 17:37:06 +0300

    fix: add map search

--- src/utils/api/getNewsDataFromApi.ts
+++
@@ -1,7 +0,0 @@
-const getNewsDataFromApi = async () => {
-  const url = 'http://api.osugame.online/fetch/';
-  const request = await fetch(url);
-  const result = await request.json();
-  return result;
-};
-export default getNewsDataFromApi;
